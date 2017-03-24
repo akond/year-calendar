bac1451d1ec8d63b728802593cf648a9d28407a0
akond <i.m.just.a.humble.tester@gmail.com>
2017-03-24 17:19:28 +0200

    Updates

--- src/year_calendar_2/event_handlers.cljs
+++ src/year_calendar_2/event_handlers.cljs
@@ -1,11 +1,12 @@
 (ns year-calendar-2.event-handlers
-	(:require [re-frame.core :as rf]))
+	(:require [re-frame.core :as rf])
+	(:import [goog object]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:mode :print-calendar
+(defonce app-state (atom {:mode               :print-calendar
 						  :__figwheel_counter 0
-						  :year 2017}))
+						  :year               2017}))
 
 (defn keep-actual [db _]
 	(reset! app-state db))
@@ -13,10 +14,14 @@
 (defn log-to-console [db _]
 	(js/console.log "STATE:" db))
 
+(defn google-analytics [db evt]
+	(when-let [gaq (goog.object.get js/window "_gaq")]
+		(.push gaq #js ["_trackEvent" "calendar" (pr-str evt)])))
+
 (def middle-ware [(rf/after keep-actual)
+				  (rf/after google-analytics)
 				  (if (identical? js/goog.DEBUG true)
-					  (rf/after log-to-console))
-				  ])
+					  (rf/after log-to-console))])
 
 
 (defn register-handlers []
